ecfdd1f5a0a1fd49ef5ca5be72e743d67fa1d4a3
«Andrey» <khomehko@mail.ru>
2024-06-17 16:15:44 +0300

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,51 @@
+//import java.util.Scanner;
+//
+//
+//public class MyFirstClass {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        byte a =127;
+//        String str ="Hello World";
+//        System.out.println("Helo OTR");
+//        System.out.println(str);
+//    }
+//}
+
+
+//-----------------
+
+import java.util.Scanner;
+
+
+//public class MyFirstClass {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        byte a =127;
+//        int age;
+//        String str ="Hello World";
+//        System.out.print("Введите число: ");
+////        System.out.println(str);
+//        age = in.nextInt();
+//        System.out.println("Лет: " + age);
+//        Scanner: in.close();
+//
+//    }
+//}
+
+//-----------------------------
+
+//public class MyFirstClass {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int gr;
+//        int kg;
+//        System.out.println("Ведите вес в граммах: " );
+//        gr = in.nextInt();
+//        System.out.println("Вес в килограммах: " + gr * 0.001 );
+//        Scanner: in.close();
+//
+//
+//    }
+//}
+
+
---
+++ src/TwoLesson.java
@@ -0,0 +1,5 @@
+public class TwoLesson {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
